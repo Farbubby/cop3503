7d77b3e559dcee9e5a6764c2a80c5d0e4af145f2
farbubby <farhanmahbub88@gmail.com>
2023-01-13 18:56:01 -0500

    implemented a function to convert string position

--- SneakyQueens.java
+++ SneakyQueens.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 public class SneakyQueens {
 
     public static void main(String[] args)
-    {
-        // Start here
+    {   
+        System.out.print();
     }
 
     public static boolean allTheQueensAreSafe(ArrayList<String> coordinateStrings, int boardSize)
@@ -15,9 +15,32 @@ public class SneakyQueens {
         return false;
     }
 
-    public static int convertStringPos(String pos)
+    public static int[] convertStringPos(String pos)
     {
-        return 0;
+        int i;
+        int horizontal = 0;
+        int vertical = 0;
+        int len = pos.length();
+        for (i = 0; i < len; i++)
+        {
+            if (Character.isDigit(pos.charAt(i))) break;
+        }
+        vertical = Integer.parseInt(pos.substring(i));
+        horizontal = baseConversion(pos);
+        int loc[] = {horizontal, vertical};
+
+        return loc;
+    }
+
+    public static int baseConversion(String pos) 
+    {
+        int i;
+        int sum = 0;
+        for (i = 0; i < pos.length(); i++)
+        {
+            if (Character.isLetter(pos.charAt(i))) sum = sum*26 + (pos.charAt(i) - 'a' + 1);
+        }
+        return sum;
     }
 
     public static double difficultyRating()
