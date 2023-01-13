b9e65fd143cbc9670a8e9b706b18aa4570e8ad11
farbubby <farhanmahbub88@gmail.com>
2023-01-13 15:38:20 -0500

    Setting up the code structure

---
+++ SneakyQueens.java
@@ -0,0 +1,33 @@
+// Farhan Mahbub
+// COP3503 Spring 2023
+// fa203667
+import java.util.ArrayList;
+
+public class SneakyQueens {
+
+    public static void main(String[] args)
+    {
+        // Start here
+    }
+
+    public static boolean allTheQueensAreSafe(ArrayList<String> coordinateStrings, int boardSize)
+    {
+        return false;
+    }
+
+    public static int convertStringPos(String pos)
+    {
+        return 0;
+    }
+
+    public static double difficultyRating()
+    {
+        return 1.0;
+    }
+
+    public static double hoursSpent()
+    {
+        return 1;
+    }
+
+}
