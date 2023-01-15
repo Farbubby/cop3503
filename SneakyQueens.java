b1da110ea75dfbb4bb4e3183dd8c6b420da5bfb6
farbubby <farhanmahbub88@gmail.com>
2023-01-15 11:22:17 -0500

    implemented cases for diagonals

--- SneakyQueens.java
+++ SneakyQueens.java
@@ -6,31 +6,43 @@ import java.util.Hashtable;
 
 public class SneakyQueens {
 
-    public static void main(String[] args)
-    {   
-        ArrayList<String> temp = new ArrayList<>();
-        temp.add("ad1");
-        temp.add("bro2");
-        temp.add("br3");
-        System.out.println(allTheQueensAreSafe(temp, 10000));
-    }
-
     public static boolean allTheQueensAreSafe(ArrayList<String> coordinateStrings, int boardSize)
     {
-        int i;
-        int point[];
-        int size = coordinateStrings.size();
-        Hashtable<Integer, Integer> xcoords = new Hashtable<>();
-        Hashtable<Integer, Integer> ycoords = new Hashtable<>();
+        int i, point[], size = coordinateStrings.size();
+        String loc;
+        Hashtable<Integer, Integer> func = new Hashtable<>(), result = new Hashtable<>();
+
         for (i = 0; i < size; i++)
         {
-            String loc = coordinateStrings.get(i);
+            loc = coordinateStrings.get(i);
             if (!(isValidPlace(loc, boardSize))) continue;
             point = convertStringPos(loc);
-            if (!(xcoords.containsKey(point[0]) || ycoords.containsKey(point[1])))
+            if (!(func.containsKey(point[0]) || result.containsKey(point[1])))
             {
-                xcoords.put(point[0], i);
-                ycoords.put(point[1], i);
+                func.put(point[0], point[1]);
+                result.put(point[1], point[0]);
+            }
+            else return false;
+        }
+        result.clear();
+        for (i = 0; i < size; i++)
+        {
+            loc = coordinateStrings.get(i);
+            point = convertStringPos(loc);
+            if (!(result.containsKey(point[0] - func.get(point[0]))))
+            {
+                result.put(point[0] - func.get(point[0]), i);
+            }
+            else return false;
+        }
+        result.clear();
+        for (i = 0; i < size; i++)
+        {
+            loc = coordinateStrings.get(i);
+            point = convertStringPos(loc);
+            if (!(result.containsKey(point[0] + func.get(point[0]))))
+            {
+                result.put(point[0] + func.get(point[0]), i);
             }
             else return false;
         }
@@ -45,23 +57,20 @@ public class SneakyQueens {
 
     public static int[] convertStringPos(String pos)
     {
-        int i;
-        int len = pos.length();
+        int i, vert, horizz, len = pos.length();
         for (i = 0; i < len; i++)
         {
             if (Character.isDigit(pos.charAt(i))) break;
         }
-        int vertical = Integer.parseInt(pos.substring(i));
-        int horizontal = baseConversion(pos);
-        int[] point = {horizontal, vertical}; 
+        vert = Integer.parseInt(pos.substring(i));
+        horizz = baseConversion(pos);
+        int[] point = {horizz, vert}; 
         return point;
     }
 
     private static int baseConversion(String pos) 
     {
-        int i;
-        int sum = 0;
-        int len = pos.length();
+        int i, sum = 0, len = pos.length();
         for (i = 0; i < len; i++)
         {
             if (Character.isLetter(pos.charAt(i))) sum = sum*26 + (pos.charAt(i) - 'a' + 1);
@@ -71,12 +80,12 @@ public class SneakyQueens {
 
     public static double difficultyRating()
     {
-        return 1.0;
+        return 2.5;
     }
 
     public static double hoursSpent()
     {
-        return 1;
+        return 15;
     }
 
 }
