b6950a28e05cbddd50b3060b304dad86f78e4d2b
farbubby <farhanmahbub88@gmail.com>
2023-01-17 12:44:50 -0500

    more comments

--- SneakyQueens.java
+++ SneakyQueens.java
@@ -12,8 +12,10 @@ public class SneakyQueens {
         Hashtable<Integer, Integer> func = new Hashtable<>(), result = new Hashtable<>();
 
         // Handles cases for a pair of queens that are on the same row or column
+        // int[][] = array of points [[x1, y1], [x2, y2], ...]
         for (i = 0; i < size; i++)
-        {
+        {   
+            // Checks if any of the x or y coords have been seen already
             if (!(func.containsKey(posList[i][0]) || result.containsKey(posList[i][1])))
             {
                 func.put(posList[i][0], posList[i][1]);
@@ -26,6 +28,7 @@ public class SneakyQueens {
         // Handles cases for a pair of queens that are on the +1 slope diagonal "/"
         for (i = 0; i < size; i++)
         {
+            // Derived from formula: (x1-y1) = a-b given (a, b) and that (x1-a)/(y1-b) = 1
             if (!(result.containsKey(posList[i][0] - func.get(posList[i][0]))))
             {
                 result.put(posList[i][0] - func.get(posList[i][0]), i);
@@ -37,6 +40,7 @@ public class SneakyQueens {
         // Handles cases for a pair of queens that are on the -1 slope diagonal "\"
         for (i = 0; i < size; i++)
         {
+            // // Derived from formula: (x1+y1) = a+b given (a, b) and that (x1-a)/(y1-b) = -1
             if (!(result.containsKey(posList[i][0] + func.get(posList[i][0]))))
             {
                 result.put(posList[i][0] + func.get(posList[i][0]), i);
@@ -46,11 +50,10 @@ public class SneakyQueens {
         return true;
     }
 
-    // int[][] = array of points [[x1, y1], [x2, y2], ...]
+    // Coverts all of the position strings into an array of point coordinates ([x1, y1])
     public static int[][] convertList(ArrayList<String> coordinateStrings)
     {   
-        int i, size = coordinateStrings.size();
-        int[][] posList = new int[size][];
+        int i, size = coordinateStrings.size(), posList[][] = new int[size][];
         for (i = 0; i < size; i++)
         {
             posList[i] = convertStringPos(coordinateStrings.get(i));
@@ -69,7 +72,7 @@ public class SneakyQueens {
         }
         vert = Integer.parseInt(pos.substring(i));
         horizz = baseConversion(pos);
-        int[] point = {horizz, vert}; 
+        int[] point = {horizz, vert};
         return point;
     }
 
