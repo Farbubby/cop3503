7afb8ada1be07001694c3a3d95bd3b1adbc9938c
farbubby <farhanmahbub88@gmail.com>
2023-01-18 21:35:18 -0500

    slightly improved memory usage

--- SneakyQueens.java
+++ SneakyQueens.java
@@ -12,49 +12,37 @@ public class SneakyQueens {
         Hashtable<Integer, Integer> xcoords = new Hashtable<>(), ycoords = new Hashtable<>();
         Hashtable<Integer, Integer> leftDown = new Hashtable<>(), rightUp = new Hashtable<>();
 
-        // Arraylist of arrays which are points ([x1, y1])
-        ArrayList<int[]> posList = convertList(coordinateStrings);
-
         for (i = 0; i < size; i++)
         {   
+            // Generates a point [x1, y1]
+            int[] point = convertStringPos(coordinateStrings.get(i));
+
             // Handles cases for a pair of queens that are on the same row or column
             // Checks if any of the x or y coords have been seen already
-            if (!(xcoords.containsKey(posList.get(i)[0]) || ycoords.containsKey(posList.get(i)[1])))
+            if (!(xcoords.containsKey(point[0]) || ycoords.containsKey(point[1])))
             {
-                xcoords.put(posList.get(i)[0], posList.get(i)[1]);
-                ycoords.put(posList.get(i)[1], posList.get(i)[0]);
+                xcoords.put(point[0], point[1]);
+                ycoords.put(point[1], point[0]);
             }
             else return false;
 
             // Handles cases for a pair of queens that are on the +1 slope diagonal "/"
             // Derived from formula: (x1-y1) = a-b given (a, b) and that (x1-a)/(y1-b) = 1
-            if (!(rightUp.containsKey(posList.get(i)[0] - xcoords.get(posList.get(i)[0]))))
+            if (!(rightUp.containsKey(point[0] - xcoords.get(point[0]))))
             {
-                rightUp.put(posList.get(i)[0] - xcoords.get(posList.get(i)[0]), i);
+                rightUp.put(point[0] - xcoords.get(point[0]), i);
             }
             else return false;
 
             // Handles cases for a pair of queens that are on the -1 slope diagonal "\"
             // Derived from formula: (x1+y1) = a+b given (a, b) and that (x1-a)/(y1-b) = -1
-            if (!(leftDown.containsKey(posList.get(i)[0] + xcoords.get(posList.get(i)[0]))))
+            if (!(leftDown.containsKey(point[0] + xcoords.get(point[0]))))
             {
-                leftDown.put(posList.get(i)[0] + xcoords.get(posList.get(i)[0]), i);
+                leftDown.put(point[0] + xcoords.get(point[0]), i);
             }
             else return false;
         }
         return true;
-    }
-
-    // Converts all of the position strings into an array of point coordinates ([x1, y1])
-    public static ArrayList<int[]> convertList(ArrayList<String> coordinateStrings)
-    {   
-        int i, size = coordinateStrings.size();
-        ArrayList<int[]> posList = new ArrayList<>();
-        for (i = 0; i < size; i++)
-        {
-            posList.add(convertStringPos(coordinateStrings.get(i)));
-        }
-        return posList;
     }
 
     // Helper function that converts the letters and digits part of the string into an array point [x1, y1]
