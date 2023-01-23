7b58f7368b162e756f2eaff6f3c93c8c50e12b94
farbubby <farhanmahbub88@gmail.com>
2023-01-22 19:10:13 -0500

    small comment fixes

--- SneakyQueens.java
+++ SneakyQueens.java
@@ -16,30 +16,39 @@ public class SneakyQueens
             // Generates a point [x1, y1]
             int[] point = convertStringPos(coordinateStrings.get(i));
 
-            // Handles cases for a pair of queens that are on the same row or column
+            // Handles cases for whether a pair of queens are on the same row or column
             // Checks if any of the x or y coords have been seen already
             if (!(xcoords.containsKey(point[0]) || ycoords.containsKey(point[1])))
             {
                 xcoords.put(point[0], point[1]);
                 ycoords.put(point[1], point[0]);
             }
-            else return false;
+            else 
+            {
+                return false;
+            }
 
-            // Handles cases for a pair of queens that are on the +1 slope diagonal "/"
+            // Handles cases for whether a pair of queens are on the +1 slope diagonal "/"
             // Derived from formula: (x1-y1) = a-b given (a, b) and that (x1-a)/(y1-b) = 1
             if (!(rightUp.containsKey(point[0] - point[1])))
             {
                 rightUp.put(point[0] - point[1], i);
             }
-            else return false;
+            else 
+            {
+                return false;
+            }
 
-            // Handles cases for a pair of queens that are on the -1 slope diagonal "\"
+            // Handles cases for whether a pair of queens are on the -1 slope diagonal "\"
             // Derived from formula: (x1+y1) = a+b given (a, b) and that (x1-a)/(y1-b) = -1
             if (!(rightDown.containsKey(point[0] + point[1])))
             {
                 rightDown.put(point[0] + point[1], i);
             }
-            else return false;
+            else 
+            {
+                return false;
+            }
         }
 
         return true;
@@ -48,37 +57,23 @@ public class SneakyQueens
     // Function that converts the letters and digits of the string into an array point [x1, y1]
     private static int[] convertStringPos(String pos)
     {
-        int i, vert, horizz, len = pos.length();
+        int i, charSum = 0, digiSum = 0, len = pos.length();
 
-        // Iterates until a digit is reached in the string
+        // Uses base-26 and base-10 calculations for the letters and digits respectively
         for (i = 0; i < len; i++)
         {
-            if (Character.isDigit(pos.charAt(i))) break;
-        }
-
-        vert = Integer.parseInt(pos.substring(i));
-        horizz = baseConversion(pos);
-        int[] point = {horizz, vert};
-        return point;
-    }
-
-    // Helper function that converts the letters into an x-coordinate with base-26 calculations
-    private static int baseConversion(String pos) 
-    {
-        int i, sum = 0, len = pos.length();
-
-        for (i = 0; i < len; i++)
-        {
-            // Stops when a digit is reached
-            // Iterative approach using the idea of (c * q) + r1: q is quotient and r1 is remainer
             if (Character.isLetter(pos.charAt(i))) 
             {
-                sum = (sum * 26) + (pos.charAt(i) - 'a' + 1);
+                charSum = (charSum * 26) + (pos.charAt(i) - 'a' + 1);
             }
-            else break;
+            else if (Character.isDigit(pos.charAt(i)))
+            {
+                digiSum = (digiSum * 10) + (pos.charAt(i) - '0');
+            }
         }
 
-        return sum;
+        int[] point = {charSum, digiSum};
+        return point;
     }
 
     public static double difficultyRating()
