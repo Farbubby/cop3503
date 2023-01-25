a228430a852b5b7d983901c2af506b77fed09184
farbubby <farhanmahbub88@gmail.com>
2023-01-24 23:13:37 -0500

    small change

--- Extras/GenericBST/GenericBST.java
+++ Extras/GenericBST/GenericBST.java
@@ -12,6 +12,7 @@
 import java.io.*;
 import java.util.*;
 
+// Node class with data type <T>
 class Node<T>
 {
 	T data;
@@ -27,62 +28,78 @@ public class GenericBST<T extends Comparable<T>>
 {
 	private Node<T> root;
 
+	// Wrapper function
 	public void insert(T data)
 	{
 		root = insert(root, data);
 	}
 
+	// Inserts a node into the BST
 	private Node<T> insert(Node<T> root, T data)
 	{
+		// Case for an empty BST
 		if (root == null)
 		{
 			return new Node<>(data);
 		}
+		// Traverse left if current node value is larger than the value going to be inserted
 		else if (data.compareTo(root.data) < 0)
 		{
 			root.left = insert(root.left, data);
 		}
+		// Traverse right if current node value is smaller than the value going to be inserted
 		else if (data.compareTo(root.data) >= 0)
 		{
 			root.right = insert(root.right, data);
 		}
-
+		
+		// Returns the inserted node from the last function call
 		return root;
 	}
 
+	// Wrapper function
 	public void delete(T data)
 	{
 		root = delete(root, data);
 	}
 
+	// Deletes a node in the BST
 	private Node<T> delete(Node<T> root, T data)
 	{
+		// Case if the BST is empty
 		if (root == null)
 		{
 			return null;
 		}
+		// Traverse left if current node value is larger than the target value
 		else if (data.compareTo(root.data) < 0)
 		{
 			root.left = delete(root.left, data);
 		}
+		// Traverse right if current node value is smaller than the target value
 		else if (data.compareTo(root.data) > 0)
 		{
 			root.right = delete(root.right, data);
 		}
+		// For when the target node is found
 		else
 		{
+			// Case for deleting a leaf node
 			if (root.left == null && root.right == null)
 			{
 				return null;
 			}
+			// Case for deleting a node with only a right child
 			else if (root.left == null)
 			{
 				return root.right;
 			}
+			// Case for deleting a node with only a left child
 			else if (root.right == null)
 			{
 				return root.left;
 			}
+			// Case for deleting a node with 2 children
 			else
 			{
 				root.data = findMax(root.left);
@@ -97,6 +114,7 @@ public class GenericBST<T extends Comparable<T>>
 	// delete() on the left subtree, and only when that subtree is not empty.
 	private T findMax(Node<T> root)
 	{
+		// Traverses to the very right of the BST
 		while (root.right != null)
 		{
 			root = root.right;
@@ -105,31 +123,38 @@ public class GenericBST<T extends Comparable<T>>
 		return root.data;
 	}
 
+	// Wrapper function
 	public boolean contains(T data)
 	{
 		return contains(root, data);
 	}
 
+	// Returns true if the BST contains a particular value, otherwise false
 	private boolean contains(Node<T> root, T data)
 	{
+		// Case if the BST is empty
 		if (root == null)
 		{
 			return false;
 		}
+		// Traverse left if current node value is larger than the target value
 		else if (data.compareTo(root.data) < 0)
 		{
 			return contains(root.left, data);
 		}
+		// Traverse right if current node value is smaller than the target value
 		else if (data.compareTo(root.data) > 0)
 		{
 			return contains(root.right, data);
 		}
+		// The target node is found
 		else
 		{
 			return true;
 		}
 	}
 
+	// Wrapper function
 	public void inorder()
 	{
 		System.out.print("In-order Traversal:");
@@ -137,6 +162,7 @@ public class GenericBST<T extends Comparable<T>>
 		System.out.println();
 	}
 
+	// Prints out the nodes of the BST through an inorder traversal
 	private void inorder(Node<T> root)
 	{
 		if (root == null)
@@ -147,6 +173,7 @@ public class GenericBST<T extends Comparable<T>>
 		inorder(root.right);
 	}
 
+	// Wrapper function
 	public void preorder()
 	{
 		System.out.print("Pre-order Traversal:");
@@ -154,6 +181,7 @@ public class GenericBST<T extends Comparable<T>>
 		System.out.println();
 	}
 
+	// Prints out the nodes of the BST through a preorder traversal
 	private void preorder(Node<T> root)
 	{
 		if (root == null)
@@ -164,6 +192,7 @@ public class GenericBST<T extends Comparable<T>>
 		preorder(root.right);
 	}
 
+	// Wrapper function
 	public void postorder()
 	{
 		System.out.print("Post-order Traversal:");
@@ -171,6 +200,7 @@ public class GenericBST<T extends Comparable<T>>
 		System.out.println();
 	}
 
+	// Prints out the nodes of the BST through a postorder traversal
 	private void postorder(Node<T> root)
 	{
 		if (root == null)
