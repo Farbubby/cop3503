2f5ef4eb9ff6281f50036f05eb2e20cce3f4931a
farbubby <farhanmahbub88@gmail.com>
2023-03-11 22:01:22 -0500

    code assignment code set up

---
+++ TopoPaths/TopoPaths.java
@@ -0,0 +1,31 @@
+// Farhan Mahbub
+// COP3503 Spring 2023
+// fa203667
+import java.util.*;
+import java.io.*;
+
+public class TopoPaths 
+{
+
+    public static int countTopoPaths(String filename)
+    {
+        return 0;
+    }
+
+    private static ArrayList<LinkedList<Integer>> getAdjacencyList(String filename)
+    {
+        Scanner scan = new Scanner(filename);
+        return null;
+    }
+
+    public static double difficultyRating()
+    {
+        return 3.0;
+    }
+
+    public static double hoursSpent()
+    {
+        return 10.0;
+    }
+
+}
