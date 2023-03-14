228fa58816b52085b82db44707803dca052bd97e
farbubby <farhanmahbub88@gmail.com>
2023-03-14 19:09:55 -0400

    added loops based off assignment patterns

--- TopoPaths/TopoPaths.java
+++ TopoPaths/TopoPaths.java
@@ -10,12 +10,16 @@ public class TopoPaths
     
     public static void main(String[] args)
     {
-        countTopoPaths("./input_files/TestCase03-graph.txt");
+        int k = countTopoPaths("./input_files/TestCase05-graph.txt");
+        System.out.println(k);
     }
 
     public static int countTopoPaths(String filename)
     {
-        int i, j, val, numVertices, numEdges, countIncoming[];
+        int i, j, val, numVertices, numEdges, num, countIncoming[];
+        boolean visited[];
+        LinkedList<Integer> vertex;
+        ArrayDeque<Integer> available = new ArrayDeque<>();
         ArrayList<LinkedList<Integer>> adjacencyList = new ArrayList<>();
 
         try
@@ -23,6 +27,7 @@ public class TopoPaths
             Scanner scan = new Scanner(new File(filename));
             numVertices = scan.nextInt();
             countIncoming = new int[numVertices];
+            visited = new boolean[numVertices];
 
             for (i = 0; i < numVertices; i++)
             {
@@ -38,6 +43,41 @@ public class TopoPaths
                     
                 adjacencyList.add(list);
             }
+
+            for (i = 0; i < numVertices; i++)
+            {
+                if (countIncoming[i] == 0)
+                {
+                    available.add(i);
+                }
+            }
+
+            for (i = 0; i < numVertices; i++)
+            {
+                if (available.size() != 1)
+                {
+                    return 0;
+                }
+
+                num = available.pop();
+                visited[num] = true;
+                vertex = adjacencyList.get(num);
+
+                for (j = 0; j < vertex.size(); j++)
+                {
+                    countIncoming[vertex.get(j)-1]--;
+                }
+
+                for (i = 0; i < numVertices; i++)
+                {
+                    if (countIncoming[i] == 0 && !visited[i])
+                    {
+                        available.add(i);
+                    }
+                }
+            }
+
+            return 1;
         }
 
         catch (Exception e)
@@ -47,6 +87,36 @@ public class TopoPaths
 
         return 0;
     }
+
+    // private static int findZero(int[] incoming)
+    // {
+    //     int i, len = incoming.length;
+
+    //     for (i = 0; i < len; i++)
+    //     {
+    //         if (incoming[i] == 0)
+    //         {
+    //             return i;
+    //         }
+    //     }
+
+    //     return -1;
+    // }
+
+    // private static int countZeroes(int[] incoming)
+    // {
+    //     int i, count = 0, len = incoming.length;
+
+    //     for (i = 0; i < len; i++)
+    //     {
+    //         if (incoming[i] == 0)
+    //         {
+    //             return count++;
+    //         }
+    //     }
+
+    //     return count;
+    // }
 
     public static double difficultyRating()
     {
