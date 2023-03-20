437e4889fc2bfc8d3a66831e3abb3d921674b33d
farbubby <farhanmahbub88@gmail.com>
2023-03-20 17:21:28 -0400

    small code fixes

--- TopoPaths/TopoPaths.java
+++ TopoPaths/TopoPaths.java
@@ -9,15 +9,16 @@ public class TopoPaths
 {
     public static int countTopoPaths(String filename)
     {
-        int i, j, k, val, numVertices, numEdges, num, countIncoming[];
+        int i, j, val, num, numVertices, numEdges, countIncoming[];
         boolean visited[];
-        LinkedList<Integer> vertex;
+        Scanner scan;
+        LinkedList<Integer> list, vertex;
         ArrayDeque<Integer> available = new ArrayDeque<>();
         ArrayList<LinkedList<Integer>> adjacencyList = new ArrayList<>();
 
         try
         {
-            Scanner scan = new Scanner(new File(filename));
+            scan = new Scanner(new File(filename));
             numVertices = scan.nextInt();
             countIncoming = new int[numVertices];
             visited = new boolean[numVertices];
@@ -25,7 +26,7 @@ public class TopoPaths
             // Creates an adjacency list from File I/O
             for (i = 0; i < numVertices; i++)
             {
-                LinkedList<Integer> list = new LinkedList<>();
+                list = new LinkedList<>();
                 numEdges = scan.nextInt();
 
                 for (j = 0; j < numEdges; j++)
@@ -38,19 +39,18 @@ public class TopoPaths
                 adjacencyList.add(list);
             }
 
-            // Starts a queue with all of the nodes with 0 incoming edges
             for (i = 0; i < numVertices; i++)
             {
-                if (countIncoming[i] == 0)
+                // Find the nodes that aren't visited with no incoming edges
+                for (j = 0; j < numVertices; j++)
                 {
-                    available.add(i);
+                    if (countIncoming[j] == 0 && !visited[j])
+                    {
+                        available.add(j);
+                    }
                 }
-            }
 
-            for (i = 0; i < numVertices; i++)
-            {
-                // Idea #1: If at least 2 vertices have 0 incoming edges, a topopath doesn't exist
-                // Idea #2: Empty list means that all of the neighbor nodes have some incoming edge
+                // If none or at least 2 vertices have 0 incoming edges, then there's no topograph
                 if (available.size() != 1)
                 {
                     return 0;
@@ -66,20 +66,9 @@ public class TopoPaths
                 {
                     countIncoming[vertex.get(j)-1]--;
                 }
-
-                // Find the nodes that aren't visited with no incoming edges
-                for (k = 0; k < numVertices; k++)
-                {
-                    if (countIncoming[k] == 0 && !visited[k])
-                    {
-                        available.add(k);
-                    }
-                }
             }
 
             // Number of topopaths can't be greater than 1
-            // To have more than one topological sort, you need to have at least 2 vertices with
-            // 0 incoming edges which would me there would be no topopaths.
             return 1;
         }
 
