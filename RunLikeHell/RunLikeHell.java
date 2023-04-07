0adf5d59f724c20517f758c7ff7821ea9e08f1b9
farbubby <farhanmahbub88@gmail.com>
2023-04-07 06:58:32 -0400

    added starting template

---
+++ RunLikeHell/RunLikeHell.java
@@ -0,0 +1,17 @@
+public class RunLikeHell
+{
+    public static int maxGains(int[] blocks)
+    {
+        
+    }
+
+    public static double difficultyRating()
+    {
+        return 1.0;
+    }
+
+    public static double hoursSpent()
+    {
+        return 5.0;
+    }
+}
